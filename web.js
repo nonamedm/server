d67348abe1d81593029401dda2ce05900183535c
nonamedm <nonamedm@naver.com>
2023-08-29 22:17:57 +0900

    cafe24 호스팅 준비2 - 포트변경

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const path = require('path');
 const app = express();
 const bodyParser = require('body-parser');
-const port = 9090;
+const port = 8001;
 const crypto = require('crypto');
 
 
