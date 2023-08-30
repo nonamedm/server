bc83fac8a534e3ecb51f4f37cfa8ecd7512adec9
nonamedm <nonamedm@naver.com>
2023-08-30 21:15:37 +0900

    빌드한것

--- researchf/src/pages/other/MyAccount.js
+++ researchf/src/pages/other/MyAccount.js
@@ -93,38 +93,15 @@ const MyAccount = () => {
                 </div>
               </div>
               <div className="col-12 col-lg-9 col-md-8">
-                <div className="contact-form1">
-                  <div className="contact-title mb-30">
-                    <h2>Get In Touch</h2>
+                <div className="myaccount-form">
+                  <div className="myaccount-title mb-30">
+                    <h2>My Projects</h2>
                   </div>
-                  <form className="contact-form-style">
-                    <div className="row">
-                      <div className="col-lg-6">
-                        <input name="name" placeholder="Name*" type="text" />
-                      </div>
-                      <div className="col-lg-6">
-                        <input name="email" placeholder="Email*" type="email" />
-                      </div>
-                      <div className="col-lg-12">
-                        <input
-                          name="subject"
-                          placeholder="Subject*"
-                          type="text"
-                        />
-                      </div>
-                      <div className="col-lg-12">
-                        <textarea
-                          name="message"
-                          placeholder="Your Message*"
-                          defaultValue={""}
-                        />
-                        <button className="submit" type="submit">
-                          SEND
-                        </button>
-                      </div>
+                  <div className="">
+                    <div>
+
                     </div>
-                  </form>
-                  <p className="form-message" />
+                  </div>
                 </div>
               </div>
             </div>
