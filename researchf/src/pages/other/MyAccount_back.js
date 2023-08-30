9e24564b45d54f7c8f953e92087bb024d076d0ea
nonamedm <nonamedm@naver.com>
2023-08-30 17:57:02 +0900

    마이페이지 작업중

--- researchf/src/pages/other/MyAccount_back.js
+++ researchf/src/pages/other/MyAccount_back.js
@@ -1,5 +1,5 @@
 import { Fragment } from "react"; 
-import { useLocation } from "react-router-dom";
+import { useLocation } from "react-router-dom"; 
 import Accordion from "react-bootstrap/Accordion";
 import SEO from "../../components/seo";
 import LayoutOne from "../../layouts/LayoutOne";
@@ -28,19 +28,15 @@ const MyAccount = () => {
           장바구니, 찜 같은거 연결하고
           전문가는 우선 상품등록만 먼저 완료하기
         */}
-        <div className="contact-area pt-100 pb-100">
+        <div className="myaccount-area pb-80 pt-100">
           <div className="container">
-            <div className="custom-row-2">
-              <div className="col-12 col-lg-4 col-md-5">
-                <div className="contact-info-wrap1">
-                  <div className="MyPage-Header" style={{
-                      left: "0px;"
-                    }}><h2>My Page</h2>
-                  </div>
+            <div className="row">
+              <div className="ms-auto me-auto col-lg-9">
+                <div className="myaccount-wrapper">
                   <Accordion defaultActiveKey="0">
                     <Accordion.Item eventKey="0" className="single-my-account mb-20">
                       <Accordion.Header className="panel-heading">
-                        My Project
+                        <span>1 .</span> Edit your account information{" "}
                       </Accordion.Header>
                       <Accordion.Body>
                           <div className="myaccount-info-wrapper">
@@ -92,7 +88,7 @@ const MyAccount = () => {
 
                     <Accordion.Item eventKey="1" className="single-my-account mb-20">
                       <Accordion.Header className="panel-heading">
-                          Purchase
+                          <span>2 .</span> Change your password
                       </Accordion.Header>
                       <Accordion.Body>
                           <div className="myaccount-info-wrapper">
@@ -125,7 +121,7 @@ const MyAccount = () => {
 
                     <Accordion.Item eventKey="2" className="single-my-account mb-20">
                       <Accordion.Header className="panel-heading">
-                          Cash
+                          <span>3 .</span> Modify your address book entries
                       </Accordion.Header>
                       <Accordion.Body>
                           <div className="myaccount-info-wrapper">
@@ -159,103 +155,7 @@ const MyAccount = () => {
                           </div>
                       </Accordion.Body>
                     </Accordion.Item>
-
-                    <Accordion.Item eventKey="3" className="single-my-account mb-20">
-                      <Accordion.Header className="panel-heading">
-                        Cash Recharge
-                      </Accordion.Header>
-                      <Accordion.Body>
-                          <div className="myaccount-info-wrapper">
-                            <div className="account-info-wrapper">
-                              <h4>My Account Information</h4>
-                              <h5>Your Personal Details</h5>
-                            </div>
-                            <div className="row">
-                              <div className="col-lg-6 col-md-6">
-                                <div className="billing-info">
-                                  <label>First Name</label>
-                                  <input type="text" />
-                                </div>
-                              </div>
-                              <div className="col-lg-6 col-md-6">
-                                <div className="billing-info">
-                                  <label>Last Name</label>
-                                  <input type="text" />
-                                </div>
-                              </div>
-                              <div className="col-lg-12 col-md-12">
-                                <div className="billing-info">
-                                  <label>Email Address</label>
-                                  <input type="email" />
-                                </div>
-                              </div>
-                              <div className="col-lg-6 col-md-6">
-                                <div className="billing-info">
-                                  <label>Telephone</label>
-                                  <input type="text" />
-                                </div>
-                              </div>
-                              <div className="col-lg-6 col-md-6">
-                                <div className="billing-info">
-                                  <label>Fax</label>
-                                  <input type="text" />
-                                </div>
-                              </div>
-                            </div>
-                            <div className="billing-back-btn">
-                              <div className="billing-btn">
-                                <button type="submit">Continue</button>
-                              </div>
-                            </div>
-                          </div>
-                      </Accordion.Body>
-                    </Accordion.Item>
-
-                    <div eventKey="4" className="single-my-account mb-20">
-                      <div className="single-my-account mb-20 accordion-item">
-                        <h2 class="panel-heading accordion-header">
-                          <button type="button" aria-expanded="false" class="accordion-button collapsed">
-                            Cash Recharge
-                          </button>
-                        </h2>
-                      </div>
-                    </div>
                   </Accordion>
-                </div>
-              </div>
-              <div className="col-12 col-lg-8 col-md-7">
-                <div className="contact-form">
-                  <div className="contact-title mb-30">
-                    <h2>Get In Touch</h2>
-                  </div>
-                  <form className="contact-form-style">
-                    <div className="row">
-                      <div className="col-lg-6">
-                        <input name="name" placeholder="Name*" type="text" />
-                      </div>
-                      <div className="col-lg-6">
-                        <input name="email" placeholder="Email*" type="email" />
-                      </div>
-                      <div className="col-lg-12">
-                        <input
-                          name="subject"
-                          placeholder="Subject*"
-                          type="text"
-                        />
-                      </div>
-                      <div className="col-lg-12">
-                        <textarea
-                          name="message"
-                          placeholder="Your Message*"
-                          defaultValue={""}
-                        />
-                        <button className="submit" type="submit">
-                          SEND
-                        </button>
-                      </div>
-                    </div>
-                  </form>
-                  <p className="form-message" />
                 </div>
               </div>
             </div>
